89556606cc9976549872e02a1952828a94951527
Nitin Kushwaha <40468154+Nitin3351@users.noreply.github.com>
2022-09-21 22:46:56 +0530

    Create Solution.py

---
+++ Step 3: Solve Problems on Arrays/Easy/8. Find the repeating and missing numbers/Solution.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+
+def missingNumber( A, N):
+    S= (N*(N+1))/2
+    for i in A:
+        S-= i
+    return int(S)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        print(missingNumber(a, n))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
