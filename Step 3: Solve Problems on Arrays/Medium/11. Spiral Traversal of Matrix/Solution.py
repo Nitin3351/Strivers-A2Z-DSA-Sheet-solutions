8369e9d7505a27337709fc1a1a66544810d3b14c
Nitin Kushwaha <40468154+Nitin3351@users.noreply.github.com>
2022-11-03 01:41:59 +0530

    Create Solution.py

---
+++ Step 3: Solve Problems on Arrays/Medium/11. Spiral Traversal of Matrix/Solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        spiral=[]
+        R= len(matrix)
+        C= len(matrix[0])
+        top=0
+        left=0
+        right= C-1
+        bottom= R-1
+        
+        while top <= bottom and left <= right:
+            for i in range(left,right+1):
+                spiral.append(matrix[top][i])
+            top+=1
+            for i in range(top, bottom+1):
+                spiral.append(matrix[i][right])
+            right-=1
+            if top <= bottom:
+                for i in range(right,left-1,-1):
+                    spiral.append(matrix[bottom][i])
+                bottom-=1
+            if left <= right:
+                for i in range(bottom,top-1,-1):
+                    spiral.append(matrix[i][left])
+                left+=1
+        return spiral
