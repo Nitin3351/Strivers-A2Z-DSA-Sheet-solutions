d7a60480e06bd61bd8c8693f7ec2a35ba4e3971d
Nitin Kushwaha <40468154+Nitin3351@users.noreply.github.com>
2022-11-15 00:35:07 +0530

    Create Solution.py

---
+++ Step 4. Binary Search/1. Binary search on 1D array/7. Count Occurrences in Sorted Array/Solution.py
@@ -0,0 +1,39 @@
+class Solution:
+	def count(self,arr, n, x):
+		low=0;high= n-1
+		cnt=0
+		idx=-1
+		while low<=high:
+		    mid= (low+high)//2
+		    if arr[mid]<x:
+		        low= mid+1
+		    elif arr[mid]>x:
+		        high= mid-1
+		    else:
+		        cnt=1
+		        idx= mid
+		        break
+		if idx>0:
+		    mid= idx-1
+    		while mid>=0 and arr[mid]==x:
+    		    cnt+=1
+    		    mid-=1
+        if idx<n-1:
+		    mid= idx+1
+    		while mid<n and arr[mid]==x:
+    		    cnt+=1
+    		    mid+=1
+    	return cnt
+
+#{ 
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n, x = list(map(int, input().strip().split()))
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.count(arr, n, x)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
