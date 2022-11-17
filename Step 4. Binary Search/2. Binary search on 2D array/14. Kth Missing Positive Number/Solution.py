06ec10757b566a3c5f124a08bd0fdc11072ba1db
Nitin Kushwaha <40468154+Nitin3351@users.noreply.github.com>
2022-11-18 00:52:35 +0530

    Create Solution.py

---
+++ Step 4. Binary Search/2. Binary search on 2D array/14. Kth Missing Positive Number/Solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        low= 0
+        high= len(arr)
+        while low<high:
+            mid= (low+high)>>1
+            if arr[mid]-mid-1<k:
+                low= mid+1
+            else:
+                high= mid
+        return high+k
